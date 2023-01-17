8ccf6f9f3891fe02710da52892e1d30d8afd94de
ask0 <toborrm0@gmail.com>
2023-01-17 16:16:18 +0300

    Pratik3 added

---
+++ src/Pratik3_Dik_Ucgende_Hipotenus_Bulma/HipotenusBul.java
@@ -0,0 +1,17 @@
+package Pratik3_Dik_Ucgende_Hipotenus_Bulma;
+
+import java.util.Scanner;
+import static java.lang.Math.sqrt;
+
+public class HipotenusBul {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x, y;
+        System.out.println("Üçgenin ilk kenarı: ");
+        x = scanner.nextDouble();
+        System.out.println("Üçgenin ikinci kenarı: ");
+        y = scanner.nextDouble();
+        double hipotenus = sqrt(x*x + y*y);
+        System.out.println("Hipotenusu= " + hipotenus);
+    }
+}
---
+++ src/Pratik3_Dik_Ucgende_Hipotenus_Bulma/UcgeninAlaniniHesapla.java
@@ -0,0 +1,20 @@
+package Pratik3_Dik_Ucgende_Hipotenus_Bulma;
+
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+public class UcgeninAlaniniHesapla {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x,y,z;
+        System.out.println("Üçgenin kenar uzunluklarını giriniz: ");
+        x = scanner.nextDouble();
+        y = scanner.nextDouble();
+        z = scanner.nextDouble();
+        double u = (x+y+z)/2;
+        double cevre = 2*u;
+        double alan = sqrt(u*(u-x)*(u-y)*(u-z));
+        System.out.println("Çevresi: " + cevre + "\nAlanı: " + alan);
+    }
+}
