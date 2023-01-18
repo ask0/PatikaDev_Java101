8b0ce2e47c6f09081109eff69d79be4255542ec8
ask0 <voidask0@gmail.com>
2023-01-18 12:52:42 +0300

    SinifGecme updated

--- src/Pratik_11_SinifGecme/SinifGecme.java
+++ src/Pratik_11_SinifGecme/SinifGecme.java
@@ -8,21 +8,32 @@ public class SinifGecme {
         double mat, fiz, tur, kim, muz;
         double gecmeNotu = 55;
         double ort;
-        System.out.print("Matematik notunu girin: ");
-        mat = scanner.nextDouble();
-        System.out.print("Fizik notunu girin: ");
-        fiz = scanner.nextDouble();
-        System.out.print("TÜrkçe notunu girin: ");
-        tur = scanner.nextDouble();
-        System.out.print("Kimya notunu girin: ");
-        kim = scanner.nextDouble();
-        System.out.print("Müzik notunu girin: ");
-        muz = scanner.nextDouble();
-        ort = (mat + fiz + tur + kim + muz) / 5;
-        System.out.print("Ortalaman: " + ort);
-        if(ort >= gecmeNotu)
-            System.out.print("\nSınıfı geçtin.");
-        else
-            System.out.print("\nSınıfta kaldın");
+        boolean flag = true;
+        while(flag){
+            System.out.print("Matematik notunu girin: ");
+            mat = scanner.nextDouble();
+            System.out.print("Fizik notunu girin: ");
+            fiz = scanner.nextDouble();
+            System.out.print("TÜrkçe notunu girin: ");
+            tur = scanner.nextDouble();
+            System.out.print("Kimya notunu girin: ");
+            kim = scanner.nextDouble();
+            System.out.print("Müzik notunu girin: ");
+            muz = scanner.nextDouble();
+            ort = (mat + fiz + tur + kim + muz) / 5;
+            if(ort >= gecmeNotu && ort<=100){
+                System.out.print("Ortalaman: " + ort);
+                System.out.print("\nSınıfı geçtin.");
+                flag = false;
+            }
+            else if(ort >= 0 && ort <gecmeNotu){
+                System.out.print("Ortalaman: " + ort);
+                System.out.print("\nSınıfta kaldın");
+                flag = false;
+            }
+            else
+                System.out.println("Ortalama 0-100 aralığında olmalıdır. Notlarını tekrar gir. ");
+        }
+
     }
 }
