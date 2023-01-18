788380868480136dd87f940458e9007bc1b570d9
ask0 <voidask0@gmail.com>
2023-01-18 13:01:08 +0300

    SinifGecme updated

--- src/Pratik_11_SinifGecme/SinifGecme.java
+++ src/Pratik_11_SinifGecme/SinifGecme.java
@@ -10,16 +10,37 @@ public class SinifGecme {
         double ort;
         boolean flag = true;
         while(flag){
+
             System.out.print("Matematik notunu girin: ");
             mat = scanner.nextDouble();
+            if(mat < 0 || mat > 100){
+                System.out.println("Girilen not 0-100 aralığında olmalıdır.");
+                break;
+            }
             System.out.print("Fizik notunu girin: ");
             fiz = scanner.nextDouble();
+            if(fiz < 0 || fiz > 100){
+                System.out.println("Girilen not 0-100 aralığında olmalıdır.");
+                break;
+            }
             System.out.print("TÜrkçe notunu girin: ");
             tur = scanner.nextDouble();
+            if(tur < 0 || tur > 100){
+                System.out.println("Girilen not 0-100 aralığında olmalıdır.");
+                break;
+            }
             System.out.print("Kimya notunu girin: ");
             kim = scanner.nextDouble();
+            if(kim < 0 || kim > 100){
+                System.out.println("Girilen not 0-100 aralığında olmalıdır.");
+                break;
+            }
             System.out.print("Müzik notunu girin: ");
             muz = scanner.nextDouble();
+            if(muz < 0 || muz > 100){
+                System.out.println("Girilen not 0-100 aralığında olmalıdır.");
+                break;
+            }
             ort = (mat + fiz + tur + kim + muz) / 5;
             if(ort >= gecmeNotu && ort<=100){
                 System.out.print("Ortalaman: " + ort);
