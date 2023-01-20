a3c3beaf2e8142e3e3c3343d990f88a980181d55
ask0 <voidask0@gmail.com>
2023-01-20 23:30:57 +0300

    RecursiveUsluSayi updated

--- src/Pratik_39_RecursiveUsluSayi/RecursiveUsluSayi.java
+++ src/Pratik_39_RecursiveUsluSayi/RecursiveUsluSayi.java
@@ -8,14 +8,30 @@ public class RecursiveUsluSayi {
             return 1;
         return taban * us(taban, kuvvet - 1);
     }
+
     static Scanner scanner = new Scanner(System.in);
+
     static void usluSayiBul() {
-        System.out.print("Taban değeri girin: ");
-        int a = scanner.nextInt();
-        System.out.print("Kuvvet değeri girin: ");
-        int b = scanner.nextInt();
-        System.out.print("Sonuç: " + us(a, b));
+        try {
+            boolean isContinue = true;
+            while (isContinue) {
+                System.out.print("Taban değeri girin: ");
+                int a = scanner.nextInt();
+                System.out.print("Kuvvet değeri girin: ");
+                int b = scanner.nextInt();
+                if (a < 0 || b < 0) {
+                    System.out.println("0'dan büyük bir sayı girin.");
+                } else {
+                    System.out.print("Sonuç: " + us(a, b));
+                    isContinue = false;
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Hatalı giriş. Sayı girin.");
+        }
     }
+
     public static void main(String[] args) {
         usluSayiBul();
     }
