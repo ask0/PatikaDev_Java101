f9fc2a303eae0157db54ee3c47c5c0cfb8b0e6d3
ask0 <voidask0@gmail.com>
2023-01-21 03:41:04 +0300

    DeseneGoreMetotOlusturma updated

--- src/Pratik_41_DeseneGoreMetotOlusturma/DeseneGoreMetotOlusturma.java
+++ src/Pratik_41_DeseneGoreMetotOlusturma/DeseneGoreMetotOlusturma.java
@@ -1,17 +1,18 @@
 package Pratik_41_DeseneGoreMetotOlusturma;
 
 import java.util.Scanner;
-
 public class DeseneGoreMetotOlusturma {
-    static String desen(int x, int y) {
-        if (x <= 0)
-            return "0";
-        return x + " " + desen(x - y, y) + " " + x;
+    static void desen (int x) {
+        System.out.print(x + " ");
+        if(x<=0)
+            return ;
+        desen(x - 5);
+        System.out.print(x + " ");
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("N sayısı: ");
         int n = scanner.nextInt();
-        System.out.println("Çıktısı: " + desen(n, 5));
+        desen(n);
     }
 }
