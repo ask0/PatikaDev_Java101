82b5dc20a36ef08c32fe7fa9658fb6521f0d87f5
ask0 <voidask0@gmail.com>
2023-01-22 21:20:54 +0300

    Updated ArraysHarmonikOrtalama

--- src/Pratik_45_ArraysHarmonikOrtalama/ArraysHarmonikOrtalama.java
+++ src/Pratik_45_ArraysHarmonikOrtalama/ArraysHarmonikOrtalama.java
@@ -1,16 +1,23 @@
 package Pratik_45_ArraysHarmonikOrtalama;
 
 import java.util.Arrays;
+
 public class ArraysHarmonikOrtalama {
     public static void main(String[] args) {
         double[] numbers = {1, 2, 3, 4, 5};
         double[] harmonic = new double[numbers.length];
 
+        double harmonicSeries;
+
         for (int i = 0; i < numbers.length; i++) {
             for (double j = 0; j < numbers[i]; j++) {
                 harmonic[i] += 1 / (j + 1);
             }
+
         }
+        harmonicSeries = harmonic[numbers.length - 1];
         System.out.println(Arrays.toString(harmonic));
+        System.out.println("Harmonic Seri: " + harmonicSeries);
+        System.out.println("Harmonic Ort: " + numbers.length / harmonicSeries);
     }
 }
