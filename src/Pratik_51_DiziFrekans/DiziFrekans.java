1b77b1cceb65f17c5e2e0f204b18a995b27e3bc8
ask0 <voidask0@gmail.com>
2023-01-23 17:10:24 +0300

    Added DiziFrekans

---
+++ src/Pratik_51_DiziFrekans/DiziFrekans.java
@@ -0,0 +1,43 @@
+package Pratik_51_DiziFrekans;
+
+import java.util.Arrays;
+
+
+public class DiziFrekans {
+    public static void main(String[] args) {
+        int[] dizi = {-5, 10, 20, 20, 0, 10, 10, 20, 5, 20, 6, 7, -5, 7, 33, 66, 99, 0, 77, 33, 33, 66, 11, 99, 99};
+        int[] frekans = new int[dizi.length];
+        Arrays.sort(dizi);
+        System.out.println(Arrays.toString(dizi));
+        System.out.println();
+        int jump;
+        int count = 0;
+        for (int i = 0; i < dizi.length; i++) {
+            int repeat = 1;
+            jump = 0;
+            for (int j = i + 1; j < dizi.length; j++) {
+                if (dizi[i] == dizi[j]) {
+                    repeat++;
+                    jump++;
+                } else break;
+            }
+            if (repeat > 0)
+                frekans[count++] = repeat;
+            i += jump;
+        }
+        // -->
+
+        count = -1;
+        for (int i = 0; i < dizi.length; i++) {
+            if (i + 1 < dizi.length) {
+                if ((dizi[i] != dizi[i + 1])) {
+                    System.out.println(dizi[i] + " sayısı " + frekans[++count] + " kere tekrar etmiştir.");
+                }
+            } else System.out.println(dizi[i] + " sayısı " + frekans[++count] + " kere tekrar etmiştir.");
+
+        }
+        // Remove 0 values from the array
+        int[] newFrequency = Arrays.copyOfRange(frekans, 0, count + 1);
+        System.out.println(Arrays.toString(newFrequency));
+    }
+}
