0f9e62dac1c29c22b00099d19a50c00e82de6c9c
ask0 <voidask0@gmail.com>
2023-01-23 17:52:04 +0300

    Updated DiziFrekans

--- src/Pratik_51_DiziFrekans/DiziFrekans.java
+++ src/Pratik_51_DiziFrekans/DiziFrekans.java
@@ -39,5 +39,26 @@ public class DiziFrekans {
         // Remove 0 values from the array
         int[] newFrequency = Arrays.copyOfRange(frekans, 0, count + 1);
         System.out.println(Arrays.toString(newFrequency));
+
+        /* Another way
+        int[] list = {100, 120, 70, 100, 10, 230, 85, 120, 100, 10, 10, -1};
+        int count;
+        System.out.println("List: " + Arrays.toString(list));
+        Arrays.sort(list);
+
+        for (int i = 0; i < list.length; i++) {
+            count = 0;
+            for (int j = 0; j < list.length; j++) {
+                if (list[i] == list[j])
+                    count++;
+            }
+            if (i == 0) {
+                System.out.println(list[i] + " repeated " + count + " times.");
+            }
+            if (i != 0 && list[i] != list[i - 1]) {
+                System.out.println(list[i] + " repeated " + count + " times.");
+            }
+        }
+        */
     }
 }
